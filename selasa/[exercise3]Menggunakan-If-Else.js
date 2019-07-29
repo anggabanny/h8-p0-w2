4603dce5798609a7e4fd28e26cdde62386966a9c
anggabanny <anggabanny@gmail.com>
2019-07-29 21:47:34 +0700

    Upload 29juli2019

---
+++ selasa/[exercise3]Menggunakan-If-Else.js
@@ -0,0 +1,23 @@
+var nama = 'angga';
+// PILIH PERAN MU WAJIB! AWALI DENGAN HURUF BESAR
+// Ksatria atau Tabib atau Penyihir
+var peran = 'Ksatria';
+
+if (nama !== '') {
+    if (peran === '') {
+        console.log("Halo " + nama + ", Pilih peranmu untuk memulai game!");
+    } else if(peran === 'Ksatria') {
+        console.log('Selamat datang di Dunia, ' + nama);
+        console.log('Halo ' + peran + ' ' + nama + ", kamu dapat menyerang dengan senjatamu!");
+    } else if(peran === 'Tabib') {
+        console.log('Selamat datang di Dunia, ' + nama);
+        console.log('Halo ' + peran + ' ' + nama + ", kamu akan membantu temanmu yang terluka.");
+    } else if(peran === 'Penyihir') {
+        console.log('Selamat datang di Dunia, ' + nama);
+        console.log('Halo ' + peran + ' ' + nama + ", ciptakan keajaiban yang membantu kemenanganmu!");
+    } else {
+        console.log("Hai "+ nama +" pilih perammu dengan benar [Ksatria] atau [Tabib] atau [Penyihir]");
+    }
+} else {
+    console.log('Nama Harus diisi!');
+}
