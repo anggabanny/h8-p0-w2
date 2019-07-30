e5841ca88deb599026f7f0704c697466a1026e69
anggabanny <anggabanny@gmail.com>
2019-07-30 15:51:36 +0700

    Upload 30Juli2019

--- rabu/[exercise6]Berlatih_Perulangan.js
+++ rabu/[exercise6]Berlatih_Perulangan.js
@@ -1,2 +1,88 @@
-[SOON]
-UPDATE
+console.log(`[1. Melakukan Looping Menggunakan While]`);
+    console.log('LOOPING PERTAMA');
+    var pertamaCount = 2;
+    while(pertamaCount <= 20){
+        console.log(pertamaCount + ' - I love coding' );
+        pertamaCount = pertamaCount + 2;
+    }
+
+    console.log('LOOPING KEDUA');
+    var pertamaCount = 20;
+    while(pertamaCount >= 2){
+        console.log(pertamaCount + ' - I will become fullstack developer' );
+        pertamaCount = pertamaCount - 2;
+    }
+
+// -----------------------------------------------
+console.log('-------------------------------------');
+// -----------------------------------------------
+
+console.log(`\n[2. Melakukan Looping Menggunakan For]`);
+    console.log('LOOPING PERTAMA');
+    for(var pertamaFor = 1; pertamaFor <= 20; pertamaFor++){
+        console.log(pertamaFor + ' - I love coding');
+    }
+
+    console.log('LOOPING KEDUA');
+    for(var keduaFor = 20; keduaFor >= 1; keduaFor--){
+        console.log(keduaFor + ' - I will become fullstack developer');
+    }
+
+// -----------------------------------------------
+console.log('-------------------------------------');
+// -----------------------------------------------
+
+console.log(`\n[3. Angka Ganjil dan Genap]`);
+    console.log('COUNTER = 1');
+    for(var i = 1; i <= 100; i = i + 1){
+        if (i % 2 === 0) {
+            console.log(i + ' Genap');
+        } else {
+            console.log(i + ' Ganjil');
+        }
+    }
+
+    console.log('\nCOUNTER = 2');
+    var a = 1;
+    while(a <= 100){
+        if (a % 3 === 0) {
+            console.log(a + ' Kelipatan 3');
+        } else {
+            console.log('"');
+        }
+        a = a + 2;
+    }
+
+    console.log('\nCOUNTER = 5');
+    var a = 1;
+    while(a <= 100){
+        if (a % 6 === 0) {
+            console.log(a + ' Kelipatan 6');
+        } else{
+            console.log('"');
+        }
+        a = a + 5;
+    }
+
+    console.log('\nCOUNTER = 9');
+    for(var j = 1; j <= 100; j = j + 9){
+        if (j % 10 === 0) {
+            console.log(j + ' Kelipatan 10');
+        } else {
+            console.log('""');
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
