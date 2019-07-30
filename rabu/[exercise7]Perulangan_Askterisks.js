4ef6024a0550394a6062dd1ba176ce0b4f879b40
anggabanny <anggabanny@gmail.com>
2019-07-30 21:33:18 +0700

    Upload 30juli2019

--- rabu/[exercise7]Perulangan_Askterisks.js
+++ rabu/[exercise7]Perulangan_Askterisks.js
@@ -1,2 +1,34 @@
-[SOON]
-UPDATE
+console.log('1. Menyusun Barisan Bintang');
+    var rows1 = 1;
+    while (rows1 <=5) {
+        console.log('*');
+        rows1+=1
+    }
+
+// --------------------------------
+console.log('-----------------------------');
+// --------------------------------
+
+console.log('\n2. Menyusun Barisan Bintang Dengan Nested Looping');
+    var rows2 = 5;
+    for(var i = 1; i <= rows2; i++){
+        var temp='';
+        for(var j = 1; j <= rows2; j++){
+            temp+='*'
+        }
+    console.log(temp);
+    }
+
+// --------------------------------
+console.log('-----------------------------');
+// --------------------------------
+
+console.log('\n3. Menyusun Barisan Tangga Bintang Dengan Nested Looping');
+    var rows3 = 5;
+    for(var k = 1; k <= rows3; k++){
+        var temp1 = '';
+        for(var l = 1; l <= k; l++){
+            temp1+='*'
+        }
+        console.log(temp1);
+    }
