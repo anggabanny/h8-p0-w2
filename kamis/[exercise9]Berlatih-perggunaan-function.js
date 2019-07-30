bd8e3fa00bba3fd4ba135fe2f72ebd3711a6ef54
anggabanny <anggabanny@gmail.com>
2019-07-30 22:41:09 +0700

    update 30juli2019

--- kamis/[exercise9]Berlatih-perggunaan-function.js
+++ kamis/[exercise9]Berlatih-perggunaan-function.js
@@ -1,1 +1,36 @@
-['SOON UPDATE']
+console.log('[TUGAS 1]');
+    function shoutOut(){
+        return 'Halo Function!'
+    }
+    console.log(shoutOut());
+    
+
+// ------------------------------------------------
+console.log("------------------------------------");
+// ------------------------------------------------
+
+console.log('\n[TUGAS 2]');
+    function calculateMultiply(num1, num2) {
+        return num1 * num2;
+    }
+    var num1 = 5;
+    var num2 = 6;
+    var hasilPerkalian = calculateMultiply(num1,num2);
+    console.log(hasilPerkalian);
+
+// ------------------------------------------------
+console.log("------------------------------------");
+// ------------------------------------------------
+
+console.log("\n[TUGAS 3]");
+    function processSentence(name,age,address,hobby){
+        return 'Nama saya '+name+", umur saya "+age+" tahun,"+" alamat saya di "+address+", dan saya punya hobby yaitu "+hobby+"!"
+    }
+
+    var name = 'Agus';
+    var age = 30;
+    var address = 'Jln. Malioboro, Yogjakarta';
+    var hobby = 'gaming';
+
+    var fullSentence = processSentence(name,age,address,hobby);
+    console.log(fullSentence);
