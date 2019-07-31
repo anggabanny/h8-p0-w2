e5fb73b051b5f02c65b3e81b771eb3f4a4cc2f1d
anggabanny <anggabanny@gmail.com>
2019-07-31 15:59:35 +0700

    update 31juli2019

--- jumat/[exercise12]Konversi-Menit.js
+++ jumat/[exercise12]Konversi-Menit.js
@@ -1,1 +1,16 @@
-['UPDATE SOON']
+function konversiMenit(menit) {
+    var ambilDepan = Math.floor(menit/60);
+    var ambilDetik = menit % 60;
+        if (ambilDetik < 10) {
+            ambilDetik = '0' + ambilDetik;
+        }
+
+    return ambilDepan + ':' + ambilDetik;
+}
+
+// TEST CASES
+console.log(konversiMenit(63)); // 1:03
+console.log(konversiMenit(124)); // 2:04
+console.log(konversiMenit(53)); // 0:53
+console.log(konversiMenit(88)); // 1:28
+console.log(konversiMenit(120)); // 2:00
--- jumat/[exercise13]X_dan_O.js
+++ jumat/[exercise13]X_dan_O.js
@@ -1,1 +1,25 @@
-['SOON UPDATE']
+function xo(str) {
+    var countX = 0;
+    var countO = 0;
+
+    for(i = 0; i <= str.length; i++){
+        if (str[i] === 'x') {
+            countX++;
+        } else if (str[i] === 'o'){
+            countO++;
+        }
+    }
+    
+    if (countX === countO) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// TEST CASES
+console.log(xo('xoxoxo')); // true
+console.log(xo('oxooxo')); // false
+console.log(xo('oxo')); // false
+console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // true
